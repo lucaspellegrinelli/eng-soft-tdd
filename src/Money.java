498fb9e4c53e92fb057a88b8dc7b8a261385d763
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2020-09-30 12:07:39 -0300

    Commit #4

--- src/Money.java
+++ src/Money.java
@@ -1,15 +1,25 @@
 abstract class Money  {
     protected int amount;
+    protected String currency;
 
-    static Dollar dollar(int amount)  {
-        return new Dollar(amount);
+    static Money dollar(int amount)  {
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
     }
 
     abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
