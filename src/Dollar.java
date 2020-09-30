9d0e1bc10aed871498e8391620148c1da3a34c98
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2020-09-30 12:10:53 -0300

    Commit #5

--- src/Dollar.java
+++
@@ -1,9 +0,0 @@
-class Dollar extends Money{
-    Dollar(int amount, String currency)  {
-        super(amount, currency);
-    }
-
-    Money times(int multiplier)  {
-        return Money.dollar(amount * multiplier);
-    }
-}
--- src/Money.java
+++ src/Money.java
@@ -1,13 +1,13 @@
-abstract class Money  {
+class Money  {
     protected int amount;
     protected String currency;
 
     static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     Money(int amount, String currency) {
@@ -15,7 +15,9 @@ abstract class Money  {
         this.currency = currency;
     }
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     String currency() {
         return currency;
@@ -23,6 +25,6 @@ abstract class Money  {
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 }
